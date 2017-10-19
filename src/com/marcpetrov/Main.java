9b783cc1df1d55c7033b9a56c1dc767f0fdf997a
Peter <marcpetrov11@gmail.com>
2017-10-19 21:09:56 +0100

    Second commit

--- src/com/marcpetrov/Main.java
+++ src/com/marcpetrov/Main.java
@@ -5,5 +5,26 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Demo!");
+
+
+        String s = null;
+
+        //System.out.println(s);
+
+        String [] titles ={"William", "Beatrice", "Lucy", "Sam"};
+        String welcome = "Welcome!";
+        char[] chars = welcome.toCharArray();
+
+        //for(int i = 0; i < chars.length; i++){
+        for(String name : titles){
+            //System.out.println(chars[i]);
+            System.out.println(name);
+        }
+
+        char lastChar = chars[chars.length - 1];
+        System.out.println(lastChar);
+
+        System.out.println("Finish!");
+
     }
 }
